5851fe0e78bbd91e723ae7ca5340492a7c461fd2
WooK1184 <119159558+WooK1184@users.noreply.github.com>
2024-11-11 09:06:56 +0900

    두수의 곱 문제 풀이

---
+++ 02.js
@@ -0,0 +1,9 @@
+function solution(num1, num2) {
+    var answer = 0;
+    if (0 <= num1, num2 <= 100) {
+        answer = num1 * num2
+    } else {
+        console.log(Error)
+    }
+    return answer;
+}
