506e591604b2992bbd30aea1c5c44bcada484537
WooK1184 <qkrtjddnr03@gmail.com>
2024-11-13 09:44:43 +0900

    두 수의 나눗셈

---
+++ 07.js
@@ -0,0 +1,8 @@
+function solution(num1, num2) {
+    var answer = 0;
+    if (0 <= num1, num2 <= 100) {
+        result1 = parseFloat(num1 / num2) * 1000;
+        answer += parseInt(result1)
+    }
+    return answer;
+}
