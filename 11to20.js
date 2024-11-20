aed15c3e99d3705f4a056371f66424c7ebe5014b
WooK1184 <119159558+WooK1184@users.noreply.github.com>
2024-11-20 09:12:50 +0900

    Update 11to20.js

--- 11to20.js
+++ 11to20.js
@@ -26,3 +26,17 @@ function solution(arr) {
 
     return sumMean
 }
+
+
+// 평균 구하기
+
+function solution(arr) {
+    var answer = 0;
+    let sum = 0;
+    let sumMean = 0;
+    for (let i = 0; i < arr.length; i++) {
+        sum += arr[i]
+    }sumMean = sum / arr.length
+
+    return sumMean
+}
