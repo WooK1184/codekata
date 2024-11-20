c218574d005337ff8717db0d436c4b2395884e29
WooK1184 <119159558+WooK1184@users.noreply.github.com>
2024-11-20 09:56:45 +0900

    Update 11to20.js

--- 11to20.js
+++ 11to20.js
@@ -65,3 +65,17 @@ function solution(n) {
     }
     return answer;
 }
+
+
+// 나머지가 1이 되는 수 찾기
+
+function solution(n) {
+    var answer = 0;
+    for (let i = 0; i <= n; i++) {
+        if (n % i == 1) {
+            answer += i
+            break;
+        }
+    }
+    return answer;
+}
