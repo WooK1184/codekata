eb0a75051e35ebe3ddd59a0175ec59a53661d1c1
WooK1184 <119159558+WooK1184@users.noreply.github.com>
2024-12-05 21:10:50 +0900

    Update 20to30.js

--- 20to30.js
+++ 20to30.js
@@ -71,3 +71,18 @@ function solution(seoul) {
     let idx = seoul.indexOf('Kim')
     return '김서방은 ' + idx + "에 있다";
 }
+
+
+// 25 나누어 떨어지는 수 찾기
+
+function solution(arr, divisor) {
+    const answer = []
+    for (let i = 0; i < arr.length; i++) {
+        if(arr[i] % divisor == 0) {
+            answer.push(arr[i])
+        } 
+    } if (answer.length === 0) {
+        answer.push(-1)
+    } answer.sort((a, b) => a - b);
+    return answer
+  }  
